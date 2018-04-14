5f3c395fd85d593af1696e60343ecdf1ff8591a0
James_K_Conley <jkconleydec2012@gmail.com>
2018-04-14 16:26:26 -0700

    Add edit feature and refactored for form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,12 @@ class ArticlesController < ApplicationController
     @articles = Article.all  
   end
 
-
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -18,11 +21,19 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
   end
-
- 
 
   private
   def article_params
